a752af94b591a819f4a0109aae0c41538baafad5
NT1999-10 <sungyikoda2213@outlook.jp>
2026-08-28 17:16:54 +0900

    design: mount tokens and icon sprite once

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import IconSprite from "./components/IconSprite.jsx";
+import "./theme/tokens.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <IconSprite />
     <App />
   </React.StrictMode>
 );

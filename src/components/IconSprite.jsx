a14520c6c9179f47dd4457b51177b7939501ebf0
NT1999-10 <sungyikoda2213@outlook.jp>
2026-08-28 17:16:44 +0900

    design: add root SVG sprite mount

---
+++ src/components/IconSprite.jsx
@@ -0,0 +1,10 @@
+import yomuSprite from "../assets/yomu-sprite.svg?raw";
+
+export default function IconSprite() {
+  return (
+    <div
+      style={{ display: "contents" }}
+      dangerouslySetInnerHTML={{ __html: yomuSprite }}
+    />
+  );
+}
